d68620baca4c962281cfea51cba7212b43108f46
Jess-xca <jessicairakoze4@gmail.com>
2025-12-21 02:50:06 +0200

    Testing: Load .env in NotekeeperApplicationTests

--- src/test/java/com/notekeeper/notekeeper/NotekeeperApplicationTests.java
+++ src/test/java/com/notekeeper/notekeeper/NotekeeperApplicationTests.java
@@ -1,13 +1,20 @@
 package com.notekeeper.notekeeper;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NotekeeperApplicationTests {
 
+	@BeforeAll
+	static void setup() {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+	}
+
 	@Test
 	void contextLoads() {
 	}
-
 }
